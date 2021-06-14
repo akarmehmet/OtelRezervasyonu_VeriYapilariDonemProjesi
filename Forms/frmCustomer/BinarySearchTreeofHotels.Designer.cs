7f29241c083ff21b389a8b6966cb7ef011d0e0e0
Mustafa Serdar KONCA <mustafaserdarkonca@gmail.com>
2021-06-14 20:56:04 +0300

    Binary Search Tree of Hotels Added.

---
+++ Forms/frmCustomer/BinarySearchTreeofHotels.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace OtelRezervasyonu_VeriYapilariDonemProjesi.Forms.frmCustomer
+{
+    partial class BinarySearchTreeofHotels
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BinarySearchTreeofHotelsLbl = new System.Windows.Forms.Label();
+            this.btnInorder = new System.Windows.Forms.Button();
+            this.btnPostorder = new System.Windows.Forms.Button();
+            this.btnPreorder = new System.Windows.Forms.Button();
+            this.listViewBinarySearch = new System.Windows.Forms.ListView();
+            this.TreeDepthLbl = new System.Windows.Forms.Label();
+            this.txtReservationNo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numberofElementsLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BinarySearchTreeofHotelsLbl
+            // 
+            this.BinarySearchTreeofHotelsLbl.AutoSize = true;
+            this.BinarySearchTreeofHotelsLbl.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinarySearchTreeofHotelsLbl.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.BinarySearchTreeofHotelsLbl.Location = new System.Drawing.Point(53, 42);
+            this.BinarySearchTreeofHotelsLbl.Name = "BinarySearchTreeofHotelsLbl";
+            this.BinarySearchTreeofHotelsLbl.Size = new System.Drawing.Size(491, 41);
+            this.BinarySearchTreeofHotelsLbl.TabIndex = 111;
+            this.BinarySearchTreeofHotelsLbl.Text = "Binary Search Tree of Hotels:";
+            // 
+            // btnInorder
+            // 
+            this.btnInorder.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnInorder.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.btnInorder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInorder.Location = new System.Drawing.Point(50, 108);
+            this.btnInorder.Name = "btnInorder";
+            this.btnInorder.Size = new System.Drawing.Size(232, 44);
+            this.btnInorder.TabIndex = 112;
+            this.btnInorder.Text = "Inorder";
+            this.btnInorder.UseVisualStyleBackColor = false;
+            // 
+            // btnPostorder
+            // 
+            this.btnPostorder.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnPostorder.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.btnPostorder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPostorder.Location = new System.Drawing.Point(313, 108);
+            this.btnPostorder.Name = "btnPostorder";
+            this.btnPostorder.Size = new System.Drawing.Size(232, 44);
+            this.btnPostorder.TabIndex = 113;
+            this.btnPostorder.Text = "Postorder";
+            this.btnPostorder.UseVisualStyleBackColor = false;
+            // 
+            // btnPreorder
+            // 
+            this.btnPreorder.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnPreorder.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.btnPreorder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPreorder.Location = new System.Drawing.Point(571, 108);
+            this.btnPreorder.Name = "btnPreorder";
+            this.btnPreorder.Size = new System.Drawing.Size(232, 44);
+            this.btnPreorder.TabIndex = 114;
+            this.btnPreorder.Text = "Preorder";
+            this.btnPreorder.UseVisualStyleBackColor = false;
+            // 
+            // listViewBinarySearch
+            // 
+            this.listViewBinarySearch.HideSelection = false;
+            this.listViewBinarySearch.Location = new System.Drawing.Point(40, 171);
+            this.listViewBinarySearch.Name = "listViewBinarySearch";
+            this.listViewBinarySearch.Size = new System.Drawing.Size(763, 222);
+            this.listViewBinarySearch.TabIndex = 115;
+            this.listViewBinarySearch.UseCompatibleStateImageBehavior = false;
+            // 
+            // TreeDepthLbl
+            // 
+            this.TreeDepthLbl.AutoSize = true;
+            this.TreeDepthLbl.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.TreeDepthLbl.Location = new System.Drawing.Point(185, 443);
+            this.TreeDepthLbl.Name = "TreeDepthLbl";
+            this.TreeDepthLbl.Size = new System.Drawing.Size(116, 22);
+            this.TreeDepthLbl.TabIndex = 116;
+            this.TreeDepthLbl.Text = "Tree Depth:";
+            // 
+            // txtReservationNo
+            // 
+            this.txtReservationNo.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.txtReservationNo.Location = new System.Drawing.Point(152, 494);
+            this.txtReservationNo.Name = "txtReservationNo";
+            this.txtReservationNo.Size = new System.Drawing.Size(186, 31);
+            this.txtReservationNo.TabIndex = 117;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(125, 547);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 44);
+            this.button1.TabIndex = 118;
+            this.button1.Text = "Preorder";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(447, 547);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 44);
+            this.button2.TabIndex = 121;
+            this.button2.Text = "Preorder";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.textBox1.Location = new System.Drawing.Point(471, 494);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 31);
+            this.textBox1.TabIndex = 120;
+            // 
+            // numberofElementsLbl
+            // 
+            this.numberofElementsLbl.AutoSize = true;
+            this.numberofElementsLbl.Font = new System.Drawing.Font("Century Gothic", 14.25F);
+            this.numberofElementsLbl.Location = new System.Drawing.Point(467, 443);
+            this.numberofElementsLbl.Name = "numberofElementsLbl";
+            this.numberofElementsLbl.Size = new System.Drawing.Size(197, 22);
+            this.numberofElementsLbl.TabIndex = 119;
+            this.numberofElementsLbl.Text = "Number of Elements:";
+            // 
+            // BinarySearchTreeofHotels
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(839, 641);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numberofElementsLbl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtReservationNo);
+            this.Controls.Add(this.TreeDepthLbl);
+            this.Controls.Add(this.listViewBinarySearch);
+            this.Controls.Add(this.btnPreorder);
+            this.Controls.Add(this.btnPostorder);
+            this.Controls.Add(this.btnInorder);
+            this.Controls.Add(this.BinarySearchTreeofHotelsLbl);
+            this.Location = new System.Drawing.Point(294, 54);
+            this.Name = "BinarySearchTreeofHotels";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "BinarySearchTreeofHotels";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label BinarySearchTreeofHotelsLbl;
+        private System.Windows.Forms.Button btnInorder;
+        private System.Windows.Forms.Button btnPostorder;
+        private System.Windows.Forms.Button btnPreorder;
+        private System.Windows.Forms.ListView listViewBinarySearch;
+        private System.Windows.Forms.Label TreeDepthLbl;
+        private System.Windows.Forms.TextBox txtReservationNo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label numberofElementsLbl;
+    }
+}
